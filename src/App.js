be4c33ab1ebf7c3bbe6e14658f06a02c63016059
Donnevtis <nevtees@gmail.com>
2022-01-07 13:40:01 +0300

    add corrections

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles/app.scss';
 
-export default function () {
-  return <div>React App</div>;
-}
+const App = () => <div>React App</div>;
+
+export default App;
